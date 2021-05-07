ca8cd352b0caa967ee057846c7a9ce4c2faf122d
alexanderm0 <a.mo@student.utwente.nl>
2021-05-07 12:48:43 +0200

    Virtual env setup

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ from pgmpy.readwrite import BIFReader
 from pgmpy import inference
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Database/networks'
 
-@app.route()
+@app.route('/')
+def index():
+    return render_template('')
+
 def openNetwork(network_string: str):
     network = BIFReader.get_model(network_string)
 
