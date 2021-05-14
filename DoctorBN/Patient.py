2ebf0c743015afcd741f5b2701d823e71116dc2c
akleinau <kleinau@ovgu.de>
2021-05-14 11:31:38 +0200

    refactor patient structure

--- DoctorBN/Patient.py
+++ DoctorBN/Patient.py
@@ -1,16 +1,9 @@
 
 class Patient:
     evidences = {}
+    targets = []
+    goals = {}
 
-    def __init__(self, evidences=None):
-        if evidences is None:
-            evidences = {}
-        self.add_evidences(evidences)
-
-    #add or update evidences
-    def add_evidences(self, map):
-        for name in map.keys():
-            self.evidences[name] = map.get(name)
 
     def print_evidences(self):
         for e in self.evidences:
--- DoctorBN/Scenario.py
+++ DoctorBN/Scenario.py
@@ -12,45 +12,44 @@ class Scenario:
     goals = {}
 
     def __init__(self, name, evidences=None, targets=None, goals=None):
-        if evidences is None:
-            self.patient = Patient()
-        else:
-            self.patient = Patient(evidences)
+
+        self.patient = Patient()
 
         self.network = Network(name)
-        if targets is not None: self.targets = targets
-        if goals is not None: self.goals = goals
+        if evidences is not None: self.patient.evidences = evidences
+        if targets is not None: self.patient.targets = targets
+        if goals is not None: self.patient.goals = goals
 
     #adds a target node
     def add_target(self, name):
-        self.targets[name] = (TargetNode(name))
+        self.patient.targets[name] = (TargetNode(name))
 
     #adds multiple targets
     def add_target_list(self, list):
         for name in list:
-            self.targets[name] = (TargetNode(name))
+            self.patient.targets[name] = (TargetNode(name))
 
     #computes the values for the targets
     def compute_targets(self):
         infer = inference.VariableElimination(self.network.model)
 
-        for t in self.targets.keys():
-            node = self.targets[t]
+        for t in self.patient.targets.keys():
+            node = self.patient.targets[t]
             node.distribution = infer.query([node.name], evidence=self.patient.evidences)
 
     def print_targets(self):
-        for t in self.targets.keys():
-            print(str(self.targets[t]))
+        for t in self.patient.targets.keys():
+            print(str(self.patient.targets[t]))
 
     def compute_relevancies_for_target(self, target):
         return relevance.get_influence_of_evidences_on_target(self.network.model,
                                                              self.patient.evidences,
-                                                             self.targets[target])
+                                                             self.patient.targets[target])
 
     def compute_explanation_of_target(self, target, filename=""):
         explanation.compute_explanation_of_target(self.network.model,
                                                   self.patient.evidences,
-                                                  self.targets[target],
+                                                  self.patient.targets[target],
                                                   filename=filename)
 
     #clique-inspired: if A,B is worse then A, then A,B,C won't be better then AC
@@ -62,13 +61,13 @@ class Scenario:
         infer = inference.BeliefPropagation(self.network.model)
         results = []
         for option in self.network.states[target]:
-            goalNames = self.goals.keys()
+            goalNames = self.patient.goals.keys()
             simEvidence = self.patient.evidences.copy()
             simEvidence[target] = option
             distribution = infer.query(list(goalNames), evidence=simEvidence)
             value = distribution.values
             for goal in distribution.variables:
-                value = value[distribution.name_to_no[goal][self.goals[goal]]]
+                value = value[distribution.name_to_no[goal][self.patient.goals[goal]]]
 
             results.append({'option': option, 'value': value})
 
