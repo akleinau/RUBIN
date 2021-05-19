1e491864acaa3e70c385bd848f4a88eb3a5641d2
akleinau <kleinau@ovgu.de>
2021-05-19 14:43:33 +0200

    add arrows to global view

--- DoctorBN/src/components/visualisations/BNvis.vue
+++ DoctorBN/src/components/visualisations/BNvis.vue
@@ -25,6 +25,13 @@ export default {
     if (this.nodes != null && this.edges != null) this.visualise()
   },
   methods: {
+    normalVect(v1, v2) {
+      let x = v1.x - v2.x
+      let y = v1.y - v2.y
+      let xq = Math.pow(x,2)
+      let yq = Math.pow(y, 2)
+      return Math.sqrt(xq + yq)
+    },
     visualise() {
       const width = 300
       const height = 150
@@ -34,10 +41,10 @@ export default {
 
       const simulation = d3.forceSimulation(nodes)
           .force("link", d3.forceLink(links).id(d => d.name))
-          .force("charge", d3.forceManyBody())
           .force('collision', d3.forceCollide().radius(20))
           .force("center", d3.forceCenter(width / 2, height / 2))
-          .force("y", d3.forceY())
+          .force("y", d3.forceY(height/2))
+
 
       var svg = d3.select(this.$refs.container)
           .append("svg")
@@ -45,13 +52,39 @@ export default {
 
       svg.selectAll('*').remove();
 
+      // Define the arrowhead marker variables
+      const markerBoxWidth = 4;
+      const markerBoxHeight = 4;
+      const refX = markerBoxWidth / 2;
+      const refY = markerBoxHeight / 2;
+      const arrowPoints = [[0, 0], [0, 4], [4, 2]];
+
+      svg
+          .append('defs')
+          .append('marker')
+          .attr('id', 'arrow')
+          .attr('viewBox', [0, 0, markerBoxWidth, markerBoxHeight])
+          .attr('refX', refX)
+          .attr('refY', refY)
+          .attr('markerWidth', markerBoxWidth)
+          .attr('markerHeight', markerBoxHeight)
+          .attr('orient', 'auto-start-reverse')
+          .append('path')
+          .attr('d', d3.line()(arrowPoints))
+          .attr('stroke', 'black');
+
       const link = svg.append("g")
           .attr("stroke", "black")
           .attr("stroke-opacity", 0.6)
-          .selectAll("line")
+          .selectAll("path")
           .data(links)
           .join("line")
-          .attr("stroke-width", 0.5);
+          .attr("stroke-width", 0.5)
+          .attr('d', link)
+          .attr('marker-end', 'url(#arrow)')
+          .attr('stroke', 'black')
+          .attr('fill', 'none');
+
 
       var colorScale = d3.scaleQuantize()
           .domain([0, 1])
@@ -75,7 +108,6 @@ export default {
           .attr("ry", 2)
 
 
-
       // Text to nodes
       const text = svg.append("g")
           .attr("class", "text")
@@ -85,27 +117,27 @@ export default {
           .enter().append("text")
 
       const textName = text.append("tspan")
-            .text(d => d.name.substring(0,10) + ": ")
+          .text(d => d.name.substring(0, 10) + ": ")
       const textState = text.append("tspan")
-          .text(d => String(d.state).substring(0,10))
+          .text(d => String(d.state).substring(0, 10))
           .attr("dy", 5)
 
       simulation.on("tick", () => {
         link
             .attr("x1", d => d.source.x)
             .attr("y1", d => d.source.y)
-            .attr("x2", d => d.target.x)
-            .attr("y2", d => d.target.y);
+            .attr("x2", d => d.target.x - 15*((d.target.x-d.source.x)/ this.normalVect(d.target,d.source)))
+            .attr("y2", d => d.target.y - 7*((d.target.y-d.source.y)/this.normalVect(d.target, d.source)));
 
         node
-            .attr("x", d => d.x -12)
-            .attr("y", d => d.y - 9);
+            .attr("x", d => d.x -12.5)
+            .attr("y", d => d.y -5);
 
-         text.attr("x", d => d.x -12) //position of the lower left point of the text
-          .attr("y", d => d.y -5 ); //position of the lower left point of the text
+        text.attr("x", d => d.x) //position of the lower left point of the text
+            .attr("y", d => d.y -1); //position of the lower left point of the text
 
-        textState.attr("x", d => d.x -11)
-        textName.attr("x", d=> d.x -11)
+        textState.attr("x", d => d.x-10)
+        textName.attr("x", d => d.x-10)
 
       });
 
