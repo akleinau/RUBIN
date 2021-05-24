d3963c190628ac39e1d83775f3c35f0c00f0e0a0
akleinau <kleinau@ovgu.de>
2021-05-24 17:26:28 +0200

    setup sugiyama

---
+++ DoctorBN/src/components/visualisations/sugiyama.vue
@@ -0,0 +1,45 @@
+<template>
+<div ref="container"/>
+</template>
+
+<script>
+import * as d3 from "d3";
+
+export default {
+name: "sugiyama",
+  data() {
+    return {
+      exEdges: [
+        {"target": "a", "source": "b"},
+        {"target": "a", "source": "c"}
+      ]
+    }
+  },
+  methods: {
+    visualise() {
+      const width = 450
+      const height = 110
+
+      let links = this.exNodes
+      let nodes = this.exEdges
+
+
+      d3.select(this.$refs.container).selectAll("*").remove()
+
+      var svg = d3.select(this.$refs.container)
+          .append("svg")
+          .attr("viewBox", [0, 0, width, height]);
+
+
+
+    }
+  }
+}
+
+
+
+</script>
+
+<style scoped>
+
+</style>
