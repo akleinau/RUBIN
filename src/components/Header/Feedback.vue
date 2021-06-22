8e9f5eb944c8afc3058b1be9929642705abc0ee0
akleinau <kleinau@ovgu.de>
2021-06-22 18:32:01 +0200

    Revert " feedback test"
    This reverts commit 0dfbeaab0e7c08cded356a35d9f6f9fe0876b674.

--- src/components/Header/Feedback.vue
+++ src/components/Header/Feedback.vue
@@ -2,13 +2,13 @@
   -- feedback form not working yet -- <br>
   If the network classified a case incorrectly, please send it as feedback. <br>
   <form>
-    <Textarea v-model="value" rows="5" style="width:100%"/>
-    <div class="p-field-checkbox">
-      <Checkbox id="checkConfig" v-model="sendConfig" :binary="true" value="true"></Checkbox>
-      <label for="checkConfig">Include network configuration</label>
-    </div>
-    <Button label="Send" @click="test()"></Button>
-  </form>
+  <Textarea v-model="value" rows="5" style="width:100%"/>
+  <div class="p-field-checkbox">
+    <Checkbox id="checkConfig" v-model="sendConfig" :binary="true" value="true"></Checkbox>
+    <label for="checkConfig">Include network configuration</label>
+  </div>
+  <Button label="Send"></Button>
+    </form>
 </template>
 
 <script>
@@ -17,21 +17,6 @@ export default {
   data() {
     return {
       sendConfig: false
-    }
-  },
-  methods: {
-    async test() {
-      const gResponse = await fetch(process.env.TRUSTIFI_URL + '/api/i/v1/email', {
-        method: 'POST',
-        headers: {
-          'x-trustifi-key': process.env.TRUSTIFI_KEY,
-          'x-trustifi-secret': process.env.TRUSTIFI_SECRET
-        },
-        json: {"recipients": [{"email": process.env.MAIL}], "title": "Test", "html": "Hello Anna"}
-      });
-      let nodeDict = await gResponse.json();
-      console.log(nodeDict)
-
     }
   }
 }
