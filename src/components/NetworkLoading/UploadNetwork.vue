fc0aa8fc940f848136bf13965486d870398edcf6
akleinau <kleinau@ovgu.de>
2021-08-21 13:45:14 +0200

    deactivate direct upload

--- src/components/NetworkLoading/UploadNetwork.vue
+++ src/components/NetworkLoading/UploadNetwork.vue
@@ -29,7 +29,6 @@ export default {
   },
   methods: {
     upload: async function () {
-      let uploadForm = document.getElementById('upload-form');
       let displayName = document.getElementById('net-name').value;
       let netDescription = document.getElementById('net-description').value;
       let fileField = this.file;
@@ -38,34 +37,6 @@ export default {
       } else if (fileField.length > 1) {
         alert("Error: Please only upload one file")
       } else {
-        //upload file to server
-        if (this.uploadToServer) {
-          let formData = new FormData();
-          formData.append('displayName', displayName);
-          formData.append('file', fileField[0]);
-          formData.append('netDescription', netDescription);
-          let options = {
-            method: 'POST',
-            body: formData,
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          }
-          delete options.headers['Content-Type'];
-          let res = await fetch('https://doctorbn-backend.herokuapp.com/uploadNetwork', options);
-          let resData = await res.json()
-          if (resData === 'error1') {
-            alert("ERROR: The network name already exists, try again");
-            uploadForm.reset()
-          } else if (resData === 'error2') {
-            alert("ERROR: The filename is already in use, try again");
-            uploadForm.reset()
-          } else if (resData === 'successful') {
-            alert("The upload was successful!");
-            uploadForm.reset()
-            this.$emit("reloadNetList");
-          }
-        }
         //open network
         let reader = new FileReader()
         reader.readAsText(fileField[0])
@@ -83,7 +54,8 @@ export default {
           }
           thisRef.$emit("loadLocalNet", localNet);
 
-          //send request to upload to server
+          if (this.uploadToServer) {
+            //send request to upload to server
           let gResponse = await fetch("https://doctorbn-backend.herokuapp.com/sendNetworkRequest", {
             method: 'POST',
             headers: {
@@ -97,6 +69,7 @@ export default {
           })
           let result = await gResponse.json();
           console.log(result)
+          }
         }
       }
     }
