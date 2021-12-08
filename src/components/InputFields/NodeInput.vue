6364aa23e809fdaecb809c45d75bac44ec218792
akleinau <kleinau@ovgu.de>
2021-12-08 18:38:06 +0100

    improve node input

--- src/components/InputFields/NodeInput.vue
+++ src/components/InputFields/NodeInput.vue
@@ -1,44 +1,47 @@
 <template>
-      <DataTable :class="{NoHeader: hideHeader}"   id="table" :value="selection" class="p-datatable-sm">
-        <Column field="name" style="padding: 0; border: 0;">
-          <template #header class="table-header" v-if="title === 'Evidence'" >
-            <span class="p-input-icon-left" >
-              <i class="pi pi-search" />
+  <DataTable :class="{NoHeader: hideHeader}" id="table" :value="selection" class="p-datatable-sm">
+    <Column field="name" style="padding: 0; border: 0;">
+      <template #header class="table-header" v-if="title === 'Evidence'">
+            <span class="p-input-icon-left">
+              <i class="pi pi-search"/>
               <InputText v-model="filters['name'].name" placeholder="Search..." style="width: 200%"/>
             </span>
-          </template>
-          <template #body="slotProps">
-            {{labels[slotProps.data.name]}}
-          </template>
-        </Column>
-        <Column field="value" class="optionCol">
-          <template v-if="changeable" #body="slotProps">
-            <Dropdown v-model="slotProps.data.selected" :options="slotProps.data.options" optionLabel="name"
-                      placeholder="slotProps.data.selected" @change="onNodeChange(slotProps.data)" >
-            </Dropdown>
-            <Button icon="pi pi-times" class="p-button-rounded p-button-secondary p-button-text p-p-0 p-m-0"
-                    @click="deleteNode(slotProps.data)" />
-          </template>
-          <template v-else #body="slotProps">
-            {{labels[slotProps.data.selected.name]}}
-          </template>
-        </Column>
-      </DataTable>
-    <!--    input dialog  -->
-    <Dialog header="  " v-model:visible="overlay" style="width: 50%" :modal="true" v-if="changeable" :closable="false">
-            <DataTable :value="overlayNodes" :scrollable="true" scrollHeight="400px" class="p-datatable-sm"
+      </template>
+      <template #body="slotProps">
+        {{ labels[slotProps.data.name] }}
+      </template>
+    </Column>
+    <Column field="value" class="optionCol">
+      <template v-if="changeable" #body="slotProps">
+        <Dropdown v-model="slotProps.data.selected" :options="slotProps.data.options" optionLabel="name"
+                  placeholder="slotProps.data.selected" @change="onNodeChange(slotProps.data)">
+        </Dropdown>
+        <Button icon="pi pi-times" class="p-button-rounded p-button-secondary p-button-text p-p-0 p-m-0"
+                @click="deleteNode(slotProps.data)"/>
+      </template>
+      <template v-else #body="slotProps">
+        {{ labels[slotProps.data.selected.name] }}
+      </template>
+    </Column>
+  </DataTable>
+  <!--    input dialog  -->
+  <Dialog header="  " v-model:visible="overlay" style="width: 80%; height: 90%; background:white" :modal="true" v-if="changeable" >
+    <div class="p-grid">
+
+
+      <DataTable :value="overlayNodes" class="p-datatable-sm p-col"
                  v-model:filters="filters" filterDisplay="menu" data-key="name">
         <template #header>
-            <div class="p-d-flex p-jc-between">
+          <div class="p-d-flex p-jc-between">
                 <span class="p-input-icon-right" style="width:100%">
-                    <InputText style="width:100%" v-model="filters['name'].value" placeholder="Search" />
+                    <InputText style="width:100%" v-model="filters['name'].value" placeholder="Search"/>
                   <i class="pi pi-search"/>
                 </span>
-            </div>
+          </div>
         </template>
         <Column field="name" :header="$t('Node')">
           <template #body="slotProps">
-            {{labels[slotProps.data.name]}}
+            {{ labels[slotProps.data.name] }}
           </template>
         </Column>
         <Column field="options">
@@ -51,32 +54,30 @@
           </template>
         </Column>
       </DataTable>
+      <div class="p-col">
+          <Button class="p-mt-1" style="background:teal; border: teal; width:100%" :label="$t('add')"
+                  @click="addNodesFromOverlay()"/>
 
-      <Listbox v-model="selected" :options="nodesToAdd" optionLabel="name" emptyMessage="..."
-        listStyle="height:250px; background:#e8e8e8" >
-        <template #option="slotProps">
-          <div>
-            {{ labels[slotProps.option.name] }}: {{ slotProps.option.selected.name }}
+        <Listbox v-model="selected" :options="nodesToAdd" optionLabel="name" emptyMessage="..."
+                 listStyle="background:#e8e8e8">
+          <template #option="slotProps">
+            <div>
+              {{ labels[slotProps.option.name] }}: {{ slotProps.option.selected.name }}
               <Button icon="pi pi-times" class="p-button-rounded p-button-secondary p-button-text xButton"
-              @click="deleteNodeFromOverlay(slotProps.option)"/>
-          </div>
-        </template>
-      </Listbox>
-      <div class="p-grid p-flex p-mt-2">
-      <Button class="p-col-6 p-mr-1 p-ml-2" style="background:teal; border: teal" :label="$t('add')"
-              @click="addNodesFromOverlay()" />
-      <Button class="p-col  p-mr-2" style="background:#56717d; border: #56717d" :label="$t('cancel')"
-              @click="resetOverlay()" />
+                      @click="deleteNodeFromOverlay(slotProps.option)"/>
+            </div>
+          </template>
+        </Listbox>
       </div>
-    </Dialog>
+    </div>
+  </Dialog>
 
   <div v-if="changeable">
-          <Button class="addButton" @click="overlay = true" v-if="title === 'Evidence'"
-              :label="$t('addEvidence')"></Button>
-              <Button class="addButton" @click="overlay = true" v-else
-              :label="$t('addOutcome')"></Button>
+    <Button class="addButton" @click="overlay = true" v-if="title === 'Evidence'"
+            :label="$t('addEvidence')"></Button>
+    <Button class="addButton" @click="overlay = true" v-else
+            :label="$t('addOutcome')"></Button>
   </div>
-
 
 
 </template>
@@ -84,28 +85,30 @@
 
 <script>
 import {FilterMatchMode} from 'primevue/api';
+
 export default {
   name: "Evidence",
   emits: ["addNodes", "deleteNode"],
   props: [
     "title",
-      "nodes",
-      "selection",
-      "changeable",
-      "hideHeader",
-      "labels"
+    "nodes",
+    "selection",
+    "changeable",
+    "hideHeader",
+    "labels"
   ],
   data() {
     return {
       overlay: false,
       selected: null,
       filters: {
-        'name': {value: null, matchMode: FilterMatchMode.STARTS_WITH}},
+        'name': {value: null, matchMode: FilterMatchMode.STARTS_WITH}
+      },
       nodesToAdd: []
     }
   },
   computed: {
-    overlayNodes: function() {
+    overlayNodes: function () {
       return this.nodes.filter(x => this.nodesToAdd.find(node => node.name === x.name) == null)
     }
   },
@@ -122,20 +125,20 @@ export default {
     addNodesFromOverlay() {
       this.$emit("addNodes", this.nodesToAdd);
       this.nodesToAdd = []
-      this.overlay=false
+      this.overlay = false
     },
     resetOverlay() {
       this.nodesToAdd = []
-      this.overlay=false
+      this.overlay = false
     },
     deleteNode(node) {
       this.$emit("deleteNode", node);
     },
-     deleteNodeFromOverlay(node) {
+    deleteNodeFromOverlay(node) {
       this.nodesToAdd = this.nodesToAdd.filter(x => x !== node)
     },
     onNodeChange(node) {
-    this.$emit("addNodes", [node])
+      this.$emit("addNodes", [node])
     }
   }
 }
@@ -151,25 +154,25 @@ export default {
   position: relative;
 }
 
-#table{
+#table {
   padding: 0;
   margin: 0
 }
 
 
-.p-datatable-thead{
+.p-datatable-thead {
   padding: 0;
   border: 0;
 }
 
 
-  .NoHeader ::v-deep(.p-datatable-thead){
+.NoHeader ::v-deep(.p-datatable-thead) {
   display: None !important
-  }
+}
 
-::v-deep(.p-dropdown-label){
-    padding-right: 0;
-    margin: 0;
+::v-deep(.p-dropdown-label) {
+  padding-right: 0;
+  margin: 0;
 }
 
 </style>
--- src/components/InputFields/TherapyInput.vue
+++ src/components/InputFields/TherapyInput.vue
@@ -1,51 +1,54 @@
 <template>
   <div style="position:relative">
 
-  <div v-if="compareConfig == null">
-          <Listbox :options="selection" :optionLabel="name" listStyle="max-height:300px"
+    <div v-if="compareConfig == null">
+      <Listbox :options="selection" :optionLabel="name" listStyle="max-height:300px"
                emptyMessage="choose therapy nodes">
         <template #option="slotProps">
           <div class="p-text-center rowContent">
             {{ labels[slotProps.option.name] }}
-          <Button icon="pi pi-times" class="p-button-rounded p-button-secondary p-button-text xButton"
-              @click="deleteNode(slotProps.option)"/>
+            <Button icon="pi pi-times" class="p-button-rounded p-button-secondary p-button-text xButton"
+                    @click="deleteNode(slotProps.option)"/>
           </div>
         </template>
       </Listbox>
       <Button id="AddButton" :label="$t('addTarget')" class="p-button-secondary" @click="overlay = true"></Button>
-  </div>
-  <div v-else>
-    current: <span v-for="sel in selection" :key="sel">{{sel.name}}, </span>
-    <br>
-    {{ compareConfig.name }}: <span v-for="sel in compareConfig.config.patient.targets" :key="sel">{{sel.name}}, </span>
-  </div>
+    </div>
+    <div v-else>
+      current: <span v-for="sel in selection" :key="sel">{{ sel.name }}, </span>
+      <br>
+      {{ compareConfig.name }}: <span v-for="sel in compareConfig.config.patient.targets"
+                                      :key="sel">{{ sel.name }}, </span>
+    </div>
 
     <!--    input dialog  -->
-    <Dialog header="  " v-model:visible="overlay" style="width:50%; height:90%" :modal="true">
-      <Listbox v-model="selected" :options="overlayNodes" optionLabel="name" emptyMessage="choose evidences to add"
-               :filter="true" filterPlaceholder="Search"
-               @change="addTarget()">
+    <Dialog header="  " v-model:visible="overlay" style="width:80%; height:90%; background:white" :modal="true">
+      <div class="p-grid">
+        <Listbox v-model="selected" class="p-col p-mt-2" :options="overlayNodes" optionLabel="name"
+                 emptyMessage="choose evidences to add"
+                 :filter="true" filterPlaceholder="Search"
+                 @change="addTarget()">
           <template #option="slotProps">
-          <div v-tooltip.bottom="'Add as target'">
-            {{ labels[slotProps.option.name] }}
-          </div>
-        </template>
-      </Listbox>
-      <Listbox v-model="selected2" :options="nodesToAdd" optionLabel="name" emptyMessage="..."
-        listStyle="background:#e8e8e8">
-        <template #option="slotProps">
-          <div>
-            {{ labels[slotProps.option.name] }}
-            <Button icon="pi pi-times" class="p-button-rounded p-button-secondary p-button-text xButton"
-              @click="deleteNodeFromOverlay(slotProps.option)"/>
-          </div>
-        </template>
-      </Listbox>
-      <div class="p-grid p-flex p-mt-2">
-        <Button class="p-col-6 p-mr-1 p-ml-2" style="background:teal; border: teal" :label="$t('add')"
-              @click="addTargetsFromOverlay()" />
-        <Button class="p-col  p-mr-2" style="background:#56717d; border: #56717d" :label="$t('cancel')"
-              @click="resetOverlay()" />
+            <div v-tooltip.bottom="'Add as target'">
+              {{ labels[slotProps.option.name] }}
+            </div>
+          </template>
+        </Listbox>
+        <div class="p-col">
+            <Button class="p-mt-1" style="background:teal; border: teal; width:100%" :label="$t('add')"
+                    @click="addTargetsFromOverlay()"/>
+
+          <Listbox v-model="selected2" :options="nodesToAdd" optionLabel="name" emptyMessage="..."
+                   listStyle="background:#e8e8e8">
+            <template #option="slotProps">
+              <div>
+                {{ labels[slotProps.option.name] }}
+                <Button icon="pi pi-times" class="p-button-rounded p-button-secondary p-button-text xButton"
+                        @click="deleteNodeFromOverlay(slotProps.option)"/>
+              </div>
+            </template>
+          </Listbox>
+        </div>
       </div>
     </Dialog>
 
@@ -59,9 +62,9 @@ export default {
   emits: ["addNodes", "deleteNode"],
   props: [
     "nodes",
-      "selection",
-      "compareConfig",
-      "labels"
+    "selection",
+    "compareConfig",
+    "labels"
   ],
   data() {
     return {
@@ -71,8 +74,8 @@ export default {
       nodesToAdd: []
     }
   },
-    computed: {
-    overlayNodes: function() {
+  computed: {
+    overlayNodes: function () {
       return this.nodes.filter(x => this.nodesToAdd.find(node => node.name === x.name) == null)
     }
   },
@@ -84,11 +87,11 @@ export default {
     addTargetsFromOverlay() {
       this.$emit("addNodes", this.nodesToAdd)
       this.nodesToAdd = []
-      this.overlay=false
+      this.overlay = false
     },
     resetOverlay() {
       this.nodesToAdd = []
-      this.overlay=false
+      this.overlay = false
     },
     deleteNode(node) {
       this.$emit("deleteNode", node)
@@ -105,14 +108,14 @@ export default {
 ::v-deep(.p-tooltip) {
   padding: 10px;
   margin-right: 10px;
-  display:None
+  display: None
 }
 
 .rowContent {
   position: relative;
   display: flex;
-justify-content: center;
-align-items: center;
+  justify-content: center;
+  align-items: center;
 }
 
 #AddButton {
