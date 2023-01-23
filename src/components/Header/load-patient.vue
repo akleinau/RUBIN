e36f0f224ec8d525f3f55923016dd72d61fd2025
akleinau <kleinau@ovgu.de>
2023-01-23 13:22:33 +0100

    add first export add prediction, explanation page improve pdf

--- src/components/Header/load-patient.vue
+++ src/components/Header/load-patient.vue
@@ -7,12 +7,13 @@
                 mode="basic" :auto=true :chooseLabel="$t('Upload')" @uploader="read($event)"
                 class="flex"/>
     <Button :label="$t('Download')" @click="exportCSV()" icon="pi pi-download" class="flex"/>
-        <Button label="pdf export" icon="pi pi-download" @click="exportPDF" class="p-button-secondary" />
+    <Button label="pdf export" icon="pi pi-download" @click="exportPDF" class="p-button-secondary"/>
   </div>
 </template>
 
 <script>
 import {useStore} from '@/store'
+import {jsPDF} from 'jspdf'
 
 export default {
   name: "load-patient",
@@ -105,7 +106,77 @@ export default {
       anchor.click();
     },
     exportPDF() {
-      console.log("export")
+      let doc = new jsPDF({unit: 'px'})
+
+      doc.setFontSize(40)
+      doc.text(20, 20, 'Patient Summary')
+      doc.setFontSize(20)
+      doc.text(20, 30, 'Patient: ' + this.Store.patient.name)
+
+      let evidence = this.Store.patient.evidence.map(x => {
+        return {
+          "Evidence": this.Store.labels[x.name],
+          "option": this.Store.option_labels[x.name][x.selected.name]
+        }
+      })
+
+      doc.table(20, 40, evidence, ['Evidence', 'option'], {autoSize: true})
+
+      let goals = this.Store.patient.goals.map(x => {
+        return {
+          "Goal": this.Store.labels[x.name],
+          "option": this.Store.option_labels[x.name][x.selected.name],
+          "direction": x.direction
+        }
+      })
+
+      doc.table(20, 100, goals, ['Goal', 'option', 'direction'], {autoSize: true})
+
+      let targets = this.Store.patient.targets.map(x => {
+        return {
+          "Target": this.Store.labels[x.name]
+        }
+      })
+
+      if (targets.length > 0) {
+        doc.table(20, 160, targets, ['Target'], {autoSize: true})
+      }
+
+      doc.addPage()
+      doc.text(20, 20, 'Predictions')
+
+      let predictions = this.Store.predictions.options.map(x => {
+        return {
+          "option": JSON.stringify(x.option)
+        }
+      })
+
+      doc.table(20, 30, predictions, ['option'], {autoSize: true})
+
+      doc.addPage()
+
+      let explanations = this.Store.explain.relevance.map(x => {
+        return {
+          "label": this.Store.labels[x.node_name],
+          "overall relevance": x.overall_relevance.toFixed(2)
+        }
+      })
+
+      doc.table(20, 30, explanations, ['label', 'overall relevance'], {autoSize: true})
+
+      doc.save('export.pdf')
+
+      /*let docHTML = new jsPDF()
+      doc.html(document.querySelector("#optionsTable"), {
+        callback: function (doc) {
+          doc.save('exportHTML.pdf');
+        },
+        x: 10,
+        y: 10,
+
+      });
+
+      */
     }
   }
 }
