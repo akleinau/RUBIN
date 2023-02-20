83e7f65e56ee7fd232bd9472830473cdef080780
akleinau <kleinau@ovgu.de>
2023-02-20 10:53:32 +0100

    enable feedback

--- src/components/Header/send-feedback.vue
+++ src/components/Header/send-feedback.vue
@@ -1,35 +1,43 @@
 <template>
   {{ $t("FeedbackInfo") }}
-  <br>
-  <div style="color: red"> Currently not working!</div>
+  <br> <br>
+  Description of the problem:
   <form>
-    <Textarea v-model="description" rows="5" style="width:100%"/>
+    <Textarea v-model="description" rows="5" class="w-full mt-2 mb-2"/>
     <div class="p-field-checkbox">
       <Checkbox id="checkConfig" v-model="sendConfig" :binary="true" value="true"></Checkbox>
-      <label for="checkConfig">{{ $t("IncludeConfig") }}</label>
+      <label class="ml-2" for="checkConfig">{{ $t("IncludeConfig") }}</label>
     </div>
-    <Button :label="$t('send')" @click="sendFeedback"></Button>
+    <Button class="mt-2" :label="$t('send')" @click="sendFeedback"></Button>
   </form>
 </template>
 
 <script>
+import {useStore} from '@/store'
+
 export default {
   name: "send-feedback",
   emits: ["sendFeedback"],
+  setup() {
+    const Store = useStore()
+    return {Store}
+  },
   data() {
     return {
-      sendConfig: false,
+      sendConfig: true,
       description: ""
     }
   },
   methods: {
     async sendFeedback() {
+      const address = "https://doctorbn-backend.herokuapp.com/"
+      //const address = "http://127.0.0.1:5000/"
       let csv = "NONE"
       if (this.sendConfig) {
         csv = this.Store.createCSVcontent();
       }
 
-      const gResponse = await fetch("https://doctorbn-backend.herokuapp.com/sendFeedback", {
+      const gResponse = await fetch(address + "sendFeedback", {
         method: 'POST',
         headers: {
           'content-type': 'application/json'
