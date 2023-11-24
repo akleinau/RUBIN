f2c6963fc7e9a38a61cfbc0104e7a52ddc492059
akleinau <56925445+akleinau@users.noreply.github.com>
2023-11-24 16:36:26 +0100

    add dutch endorisk url

--- src/App.vue
+++ src/App.vue
@@ -19,8 +19,16 @@ export default {
   },
   created() {
     document.title = "RUBIN"
+
+    //dirty encoding of direct links to endorisk, and endorisk in dutch
     if (window.location.search === "?network=endorisk") {
       this.loadNetwork("endometrial cancer")
+    }
+    if (window.location.search === "?network=endorisk?lang=nl") {
+          this.loadNetwork("endometrial cancer")
+          this.$i18n.locale = "nl"
+          // eslint-disable-next-line
+          umami.track('language-change', {language: "nl", network: "endometrial cancer"} );
     }
   },
   data() {
